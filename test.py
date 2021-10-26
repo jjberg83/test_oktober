42db84e4a901b85e26a933e85648704c27315a3b
Jørund Jenssen Berg <jjberg@gmail.com>
2021-10-26 14:15:40 +0200

    La til test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 14:13:31 2021
+
+@author: jjberg
+"""
+
+if __name__ == '__main__':
+    print('Dette er et nytt script')
