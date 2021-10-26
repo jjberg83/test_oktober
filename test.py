6f713391c916f60d37e67c50e82a1954295908cf
Jørund Jenssen Berg <jjberg@gmail.com>
2021-10-26 14:30:25 +0200

    Legger til det som skal gjoeres inn i funksjon

--- test.py
+++ test.py
@@ -6,5 +6,11 @@ Created on Tue Oct 26 14:13:31 2021
 @author: jjberg
 """
 
+def ask_and_strip():
+    setning = input('Skriv en setning:\n')
+    stripped = setning.strip('!:-,.')
+    return stripped
+
 if __name__ == '__main__':
     print('Dette er et nytt script')
+    print(ask_and_strip())
